afb38dfd974cf6361077cda02821daec83c0931c
JuanFernandoBallesterosMaciasUIS <juan2224653@correo.uis.edu.co>
2023-11-07 11:39:46 -0500

    Cifrado Cesar terminado

--- cifradoCesar2.gd
+++ cifradoCesar2.gd
@@ -1,7 +1,7 @@
 extends Control
 
+var abc = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -9,3 +9,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_button_pressed():
+	var texto = $HBoxContainer2/TextEdit.text
+	var cifrado = ""
+	print(texto)
+	
+	for i in texto:
+		for j in range(len(abc)):
+			if i == abc[j]:
+				cifrado += abc[(j + 3) % 27]
+	
+	print(cifrado)
+	$HBoxContainer2/TextEdit2.text = cifrado
+
+
+
+func _on_button_2_pressed():
+	var texto = $HBoxContainer2/TextEdit.text
+	var cifrado = ""
+	
+	for i in texto:
+		for j in range(len(abc)):
+			if i == abc[j]:
+				cifrado += abc[(j - 3) % 27]
+	
+	$HBoxContainer2/TextEdit2.text = cifrado

738e04cf8b107c633982bb9423ae5f4e6ae07d3a
JuanPabloBallesterosMaciasUis <juan2224649@correo.uis.edu.co>
2023-11-13 21:29:36 -0500

    avance en el codigo Yuans

---
+++ Yuans.gd
@@ -0,0 +1,57 @@
+extends Button
+
+# Preload the images (assuming you have a limited set of letters/images)
+var images = {
+	"A": preload("res://imagenes/A.jpg"),
+	"B": preload("res://imagenes/B.png"),
+	"C": preload("res://imagenes/C.png"),
+	"D": preload("res://imagenes/D.png"),
+	"E": preload("res://imagenes/E.png"),
+	"F": preload("res://imagenes/F.png"),
+	"G": preload("res://imagenes/G.png"),
+	"H": preload("res://imagenes/H.png"),
+	"I": preload("res://imagenes/I.png"),
+	"J": preload("res://imagenes/J.png"),
+	"K": preload("res://imagenes/K.png"),
+	"L": preload("res://imagenes/L.png"),
+	"M": preload("res://imagenes/M.png"),
+	"N": preload("res://imagenes/N.jpg"),
+	"Ñ": preload("res://imagenes/Ñ.png"),
+	"O": preload("res://imagenes/O.png"),
+	"P": preload("res://imagenes/P.png"),
+	"Q": preload("res://imagenes/Q.png"),
+	"R": preload("res://imagenes/R.png"),
+	"S": preload("res://imagenes/S.jpg"),
+	"T": preload("res://imagenes/T.png"),
+	"U": preload("res://imagenes/U.jpg"),
+	"V": preload("res://imagenes/V.png"),
+	"W": preload("res://imagenes/W.png"),
+	"X": preload("res://imagenes/X.png"),
+	"Y": preload("res://imagenes/Y.jpg"),
+	"Z": preload("res://imagenes/Z.png")
+	}
+
+func _ready():
+	connect("pressed", self, "_on_button_pressed")
+
+func _on_button_pressed():
+		# Obtiene el texto ingresado y lo convierte a mayúsculas
+	var texto_ingresado = $TextEdit.text.to_upper()  # Suponiendo que la ruta al nodo de TextEdit es `TextEdit`
+	# Suponiendo que la ruta al contenedor es `VBoxContainer`
+	var contenedor = $VBoxContainer
+	# Elimina todas las imágenes existentes en el contenedor
+	contenedor.queue_free_children()
+	
+	# Itera sobre cada letra del texto ingresado
+	for letra in texto_ingresado:
+		# Verifica si existe una imagen para la letra actual
+		if imagenes.has(letra):
+			# Obtiene la textura correspondiente a la letra
+			var textura_imagen = imagenes[letra]
+			var imagen = TextureRect.new()
+			imagen.texture = textura_imagen
+			# Añade la imagen al contenedor
+			contenedor.add_child(imagen)
+		else:
+			# Si no existe una imagen para la letra, imprime un mensaje en la consola
+			print("No existe imagen para la letra: " + letra)

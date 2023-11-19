2dec277f08536bb641868b52b1971041703fb063
JuanPabloBallesterosMaciasUis <juan2224649@correo.uis.edu.co>
2023-11-19 17:08:32 -0500

    Ya funciona bien el cifrado Yuans

--- Yuans.gd
+++ Yuans.gd
@@ -31,14 +31,33 @@ var images = {
 	"Z": preload("res://imagenes/Z.png")
 	}
 
+func _ready():
+	# Configura el ScrollContainer y el HScrollBar aquí si es necesario
+	var scroll_container = $ScrollContainer
+	var h_scroll_bar = $HScrollBar
+
+	# Conecta la señal 'value_changed' del HScrollBar al método que manejará el desplazamiento
+	h_scroll_bar.connect("value_changed", Callable(self, "_on_h_scroll_bar_value_changed"))
+
+	# No es necesario conectar la señal 'ready' para HBoxContainer, ya que debería estar listo
+	# Si necesitas realizar alguna configuración inicial para HBoxContainer, puedes hacerlo aquí
+
+func _on_h_scroll_bar_value_changed(value):
+	# Cambia la posición de desplazamiento del ScrollContainer
+	$ScrollContainer.scroll_horizontal = value
+
 func _on_button_pressed():
+	var contenedor = $ScrollContainer/HBoxContainer  # Obtiene la referencia correcta al HBoxContainer
+	if contenedor == null:
+		print("El nodo HBoxContainer no está disponible o no se ha inicializado.")
+		return
+
 	# Obtiene el texto ingresado y lo convierte a mayúsculas
-	var texto_ingresado = $TextEdit.text.to_upper()  # Suponiendo que la ruta al nodo de TextEdit es `TextEdit`
-	# Suponiendo que la ruta al contenedor es `HBoxContainer`
-	var contenedor = $HBoxContainer
+	var texto_ingresado = $TextEdit.text.to_upper()
+
 	# Elimina todas las imágenes existentes en el contenedor
 	for child in contenedor.get_children():
-		child.queue_free()
+		child.queue_free()  # Esto eliminará cada nodo hijo de contenedor
 
 	# Itera sobre cada letra del texto ingresado
 	for letra in texto_ingresado:
@@ -53,6 +72,8 @@ func _on_button_pressed():
 		else:
 			# Si no existe una imagen para la letra, imprime un mensaje en la consola
 			print("No existe imagen para la letra: " + letra)
+	
+	
 
 
 
